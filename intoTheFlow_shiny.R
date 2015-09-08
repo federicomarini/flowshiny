f231d38776b71fdc78c74702a2b71af66b5fc473
Federico Marini <marinif@uni-mainz.de>
2015-09-08 16:55:22 +0200

    Initial commit

---
+++ intoTheFlow_shiny.R
@@ -0,0 +1,7 @@
+# correspondent version of bioconductor
+source("http://bioconductor.org/biocLite.R")
+# install latest shiny
+install.packages("shiny")
+# install flowcatchR and its dependencies
+biocLite("flowcatchR")
+
